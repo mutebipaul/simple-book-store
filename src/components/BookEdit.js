0d5f424a69200c6ca8c20dc6847a1206efb41f3e
mutebipaul <johnpaulmutebi@gmail.com>
2023-07-10 15:27:15 +0300

    'edit-form'

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 function BookEdit() {
-    return (<div>BookEdit</div>);
+    const[title,setTitle] = useState([]);
+
+    const handleChange =(event) =>{
+        setTitle(event.target.value);
+    }
+
+    const handleFormSubit = (event) =>{
+        event.preventDefault();
+        setTitle('');
+    }
+
+    return (<form onSubmit={handleFormSubit} className="book-edit">
+        <label>Title</label>
+        <input value = {title} onChange={handleChange} className="input"/>
+        <button className="button is-primary">save</button>
+    </form>);
 }
 export default BookEdit;
